809cecd6edf09611245645820a07528ff66a798c
Jeffrey Lubow <jml@dabkitsch.com>
2014-11-15 17:53:00 -0800

    updated to be more modular.  separate functions for each portion of functionality

--- source/scanfiles.php
+++ source/scanfiles.php
@@ -2,6 +2,8 @@
 
 include 'OSC.php';
 
+define('LABEL_DIRS', 1); //determines whether or not labels will be created for subdirs where patchers live
+
 $header = array("Name", "Description", "Authors", "Version");
 $header_spacing = 200;
 $offset_left = 10;
@@ -13,126 +15,55 @@ $textwidth = 180;
 //osc code
 $c = new OSCClient();
 $c->set_destination("localhost", 8000);
+//example:
 //$a = new OSCMessage("/foo", array(1, 2.94, "bar"));
 //$c->send($a);
 
-//copy the overview that was just populated
-function copy_overview(){
-    //this can also be in the bash script
-}
+function get_patches($dir, $flags){
+    global $c, $inc, $offset_left, $offset_top;
 
-function get_patches($dir){
-    $outpath = getcwd() . '/generated_depot_files/';
-    $in_dir = 'none';
-    $currdir = 'none';
-    $in_tutorials = $in_tutors = $in_patchers = $in_applications = $in_demos = $in_javascript = false;
-    $thisdir = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
-    $files = new RecursiveIteratorIterator($thisdir, RecursiveIteratorIterator::SELF_FIRST);
-    global $inc;
-    //global $tab_offset;
+    $dir_array = [];
+    $inc = 0;
+    $path = realpath($dir);
+    //$dirs = new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS);
+    $dirs = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
+    $objects = new RecursiveIteratorIterator($dirs, RecursiveIteratorIterator::SELF_FIRST);
+    $section = basename($dir);
+    script_header($section);
 
-    foreach($files as $object){
-	//if this is a directory... find out which one it is.
-	if($object->isDir()){
-
-	    //figure out if we have changed directories
-	    $currdir = realpath($object->getPath());
-
-	    if(strpos($currdir, '.') == false){
-		$test = strcmp($currdir, $prevdir);
-		if($test){
-		    echo "current dir changing: ", $currdir, "\n";
-		    $prevdir = $currdir;
-		}
-	    }
-
-	    if(strpos($object->getPathname(), 'extras/tutorials') == true){
-		$in_dir = 'tutorials';
-		if(!$in_tutorials){
-		    echo "in tutorials \n";
-		    script_header($in_dir);
-		    $in_tutorials = true;
-		    $tab_offset = true;
-		    $inc = 0;
-		}else{
-		    $tab_offset = false;
-		}
-	    }
-	    else if(strpos($object->getPathname(), 'extras/tutors') == true){
-		$in_dir = 'tutors';
-		if(!$in_tutors){
-		    echo "in tutors \n";
-		    script_header($in_dir);
-		    $in_tutors = true;
-		    $tab_offset = true;
-		    $inc = 0;
-		}
-	    }else if(strpos($object->getPathname(), 'javascript') == true){
-		$in_dir = 'javascript';
-		if(!$in_javascript){
-		    echo "in javascript \n";
-		    script_header($in_dir);
-		    $in_javascript = true;
-		    $tab_offset = true;
-		    $inc = 0;
-		}else{
-		    $tab_offset = false;
-		}
-	    }else if(strpos($object->getPathname(), 'patchers') == true){
-		$in_dir = 'patchers';
-		if(!$in_patchers){
-		    echo "in patchers  \n";
-		    script_header($in_dir);
-		    $in_patchers = true;
-		    $tab_offset = true;
-		    $inc = 0;
-		}
-	    }else if(strpos($object->getPathname(), 'examples/applications') == true){
-		$in_dir = 'applications';
-		if(!$in_applications){
-		    echo "in applications \n";
-		    script_header($in_dir);
-		    $in_applications = true;
-		    $tab_offset = true;
-		    $inc = 0;
-		}
-	    }else if(strpos($object->getPathname(), 'examples/demos') == true){
-		$in_dir = 'demos';
-		if(!$in_demos){
-		    echo "in demos  \n";
-		    script_header($in_dir);
-		    $in_demos = true;
-		    $tab_offset = true;
-		    $inc = 0;
-		}
-	    }
-	}else{
-	    switch($in_dir){
-		case 'tutorials':
-		    check_file($object, $in_dir);
-		    break;
-		case 'tutors':
-		    check_file($object, $in_dir);
-		    break;
-		case 'patchers':
-		    check_file($object, $in_dir);
-		    break;
-		case 'applications':
-		    check_file($object, $in_dir);
-		    break;
-		case 'demos':
-		    check_file($object, $in_dir);
-		    break;
-		case 'javascript':
-		    check_file($object, $in_dir);
-		    break;
-	    }
+    // filter unique paths: loop through all objects and store names in dir_array
+    foreach($objects as $name => $object){
+	if($object->isDir() && $object->getBasename()[0] != '.'){
+	    array_push($dir_array, $name);
 	}
     }
 
-    //if ((($ext == "maxhelp") || ($ext == "maxpat")) && (check_banner_badge($file) == true)) {
-    //	extract_data($file);
-    //}
+    array_push($dir_array, $dir);
+    //print_r($dir_array);
+
+    //check for banner/badges for each folder as a recursive process with filtered array of paths
+    for($i = 0; $i < count($dir_array); $i++){
+
+	if($flags == 1){
+	    script_label(basename($dir_array[$i]), $section);
+	}
+
+	foreach(new DirectoryIterator($dir_array[$i]) as $file){
+	    if($file->isFile() && $file->getBasename()[0] != '.'){
+		check_file($file->getRealPath(), $section);
+	    }
+	}
+    }
+}
+	    
+function script_label($text, $section){
+    global $c, $offset_left, $offset_top, $inc;
+    $pad_y = 40;
+    $y = ($inc * 50) + ($offset_top + $pad_y);
+    $str = "script newobject comment @text " . $text . " @fontface 2 @underline 1 @fixwidth 1 @patching_rect " . $offset_left . " " . $y;
+    $a = new OSCMessage("/" . $section, array($str));
+    $c->send($a);
+    $inc++;
 }
 
 function script_header($section){
@@ -147,131 +78,110 @@ function script_header($section){
 	}
 }
 
+//cleanup these two functions
 function check_file($infile, $section){
     if(check_banner_badge($infile)){
-	extract_data($infile, $section);
+	extract_to_osc($infile, $section);
     }
 }
-
 function check_banner_badge($patchfile){
-    $test = implode("", file($patchfile));
+    $test = file_get_contents($patchfile);
+    //$test = implode("", file($patchfile));
     $obj = json_decode($test, true);
     $outtest = false;
     $is_banner = false;
-    $is_badge = false;
+    //$is_badge = false;
     
     for ($i = 0; $i <= sizeof($obj['patcher']['boxes']); $i++) {
-	if ($obj['patcher']['boxes'][$i]['box']['name'] == "badge.maxpat") {
+	/*if ($obj['patcher']['boxes'][$i]['box']['name'] == "badge.maxpat") {
 		$is_badge = true;
-	}
+	}*/
 	if ($obj['patcher']['boxes'][$i]['box']['name'] == "banner.maxpat") {
 		$is_banner = true;
 	}
     }
-
     if($is_banner) {
-	    $outtest = true;
+	$outtest = true;
     }
     return $outtest;
 }
 
-function extract_data($thisfile, $section){
-    global $header_spacing, $offset_left, $inc, $c, $sec, $currid, $textwidth;
-    $test = implode("", file($thisfile));
-
-    //print 'current file:' . $thisfile . "\n";
-
+function extract_to_osc($thisfile, $section){
+    global $header_spacing, $offset_left, $inc, $c, $currid, $textwidth;
+    $test = file_get_contents($thisfile);
     $obj = json_decode($test, true);
-    $location = -1;
+    $xloc = -1;
     $inc++;
 
+    $y = ($inc * 50) + -10;
+
     for ($i = 0; $i <= sizeof($obj['patcher']['boxes']); $i ++) {
-	//initial loop looks for banner name/description
+
+	//search for banner name/description
 	if ($obj['patcher']['boxes'][$i]['box']['name'] == "banner.maxpat") {
-	    $name = basename($thisfile);
-	    //could also do: 
-	    //$name = $obj['patcher']['boxes'][$i]['box']['args'][0];
-
-	    $description = $obj['patcher']['boxes'][$i]['box']['args'][1];
-	    $location = 0;
-	    $currid += 1;//inc before you use
-	    $varname = 'name-' . $currid;
-	    $x = ($location * $header_spacing) + $offset_left;
-	    $y = ($inc * 50) + -10;
-
-	    $str = 'script newobject textbutton @fontsize 11 @truncate 0 @border 0 @ignoreclick 0 @fontface bold @varname 
-		' . $varname . ' @patching_rect ' . $x . ' ' . $y . ' ' . $textwidth . ' 40';
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
-
-	    $str = 'script send ' . $varname . ' text ' . '"' . $name . '"';
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
-
-	    $str = 'script hidden connect ' . $varname . ' 1 peewee 0';
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
-
-	    $location = 1;
-	    $currid += 1;//inc before you use
-	    $varname = 'desc-' . $currid;
-
-	    //echo 'banner/description var: ' . $description . "\n";
-	    $x = ($location * $header_spacing) + $offset_left;
-
-	    $str = 'script newobject textbutton @fontsize 10 @truncate 0 @border 0 @ignoreclick 1 @varname 
-		' . $varname . ' @patching_rect ' . $x . ' ' . $y . ' ' . $textwidth . ' 40';
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
-
-	    $str = 'script send ' . $varname . ' text ' . '"' . $description . '"';
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
+	    //name
+	    $name_text = basename($thisfile);
+	    $varname = 'name-' . $currid += 1;
+	    osc_script_objects($name_text, $varname, 0, $y, 'name', $section);
+	    //description
+	    $desc_text = $obj['patcher']['boxes'][$i]['box']['args'][1];
+	    $varname = 'desc-' . $currid += 1;
+	    osc_script_objects($desc_text, $varname, 1, $y, 'description', $section);
+	}
+	if ($obj['patcher']['boxes'][$i]['box']['name'] == "badge.maxpat") {
+	    //authors
+	    $author_text = $obj['patcher']['boxes'][$i]['box']['args'][2];
+	    $varname = 'auth-' . $currid += 1;
+	    osc_script_objects($author_text, $varname, 2, $y, 'authors', $section);
+	    //version
+	    $version_text = $obj['patcher']['boxes'][$i]['box']['args'][1];
+	    $varname = 'vers-' . $currid += 1;
+	    osc_script_objects($version_text, $varname, 3, $y, 'version', $section);
 	}
     }
-
-    //secondary for loop ensures that the badge info will get output second (order can be diff in json)
-    for ($i = 0; $i <= sizeof($obj['patcher']['boxes']); $i ++) {
-	if ($obj['patcher']['boxes'][$i]['box']['name'] == "badge.maxpat") {
-	    $author = $obj['patcher']['boxes'][$i]['box']['args'][2];
-	    $version = $obj['patcher']['boxes'][$i]['box']['args'][1];
-	    $location = 2;
-	    $currid += 1;
-	    $varname = 'auth-' . $currid;
-
-	    //echo 'author var: ' . $author . "\n";
-	    $x = ($location * $header_spacing) + $offset_left;
-
-	    $str = 'script newobject textbutton @fontsize 10 @truncate 0 @border 0 @ignoreclick 1 @varname 
-		' . $varname . ' @patching_rect ' . $x . ' ' . $y . ' ' . $textwidth . ' 40';
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
-
-	    $str = 'script send ' . $varname . ' text ' . '"' . $author . '"';
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
-
-	    $location = 3;
-	    $currid += 1;
-	    $varname = 'vers-' . $currid;
-
-	    //echo 'version var: ' . $version . "\n";
-	    $x = ($location * $header_spacing) + $offset_left;
-
-	    $str = 'script newobject textbutton @fontsize 10 @truncate 0 @border 0 @ignoreclick 1 @varname 
-		' . $varname . ' @patching_rect ' . $x . ' ' . $y . ' ' . ($textwidth / 2) . ' 40';
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
-	    $str = 'script send ' . $varname . ' text ' . $version;
-	    $a = new OSCMessage("/" . $section, array($str)); 
-	    $c->send($a);
-	}
-    }	 
 }
 
-//open_overview();
-get_patches('../');
-//copy_overview();
+function osc_script_objects($content, $vname, $loc_id, $y, $case, $section){
+    global $c, $header_spacing, $offset_left, $textwidth;
+
+    $x = ($loc_id * $header_spacing) + $offset_left;
+    $width = $textwidth;
+
+    if($case == 'name'){
+	$fontsize = 11;
+	$fontface = 'bold';
+	$ignoreclick = 0;
+    }else{
+	$fontsize = 10;
+	$fontface = 'regular';
+	$ignoreclick = 1;
+    }
+    if($case == 'version'){
+	$width = $textwidth / 2;
+    }
+
+    $str = 'script newobject textbutton @fontsize ' . $fontsize . ' @truncate 0 @border 0 @ignoreclick ' . $ignoreclick . ' @fontface ' .
+	$fontface . ' @varname ' . $vname . ' @patching_rect ' . $x . ' ' . $y . ' ' . $width . ' 40';
+    $a = new OSCMessage("/" . $section, array($str)); 
+    $c->send($a);
+
+    $str = 'script send ' . $vname . ' text ' . '"' . $content . '"';
+    $a = new OSCMessage("/" . $section, array($str)); 
+    $c->send($a);
+
+    if($case == 'name'){
+	$str = 'script hidden connect ' . $vname . ' 1 peewee 0';
+	$a = new OSCMessage("/" . $section, array($str)); 
+	$c->send($a);
+    }
+
+}
+
+get_patches('../javascript', 0);
+get_patches('../extras/tutorials', 0);
+get_patches('../extras/tutors', 0);
+get_patches('../examples/applications', 0);
+get_patches('../examples/demos', 0);
+get_patches('../patchers', 1);
 
 ?>	
-
